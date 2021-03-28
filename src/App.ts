7c9257b4cd5abde60054312075670c46487a29fa
zsh2401 <zsh2401@163.com>
2021-03-28 16:46:55 +0800

    添加一段令人绝望的注释

--- src/App.ts
+++ src/App.ts
@@ -8,6 +8,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Router from './Router'
 
+/* 2021年3月27日项目启动
+ 2021年3月28日已经有一半代码看不懂了
+ 希望将来的某一天，需要修改时，还有机会吧！
+ */
 export default function() {
     ReactDOM.render(React.createElement(Router)
         , document.querySelector("#app"));
