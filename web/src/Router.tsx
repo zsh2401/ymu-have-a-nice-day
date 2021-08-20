ab77f06c5caaff8a661796aad807e28e782e8697
zsh2401 <zsh2401@163.com>
2021-08-20 21:59:21 +0800

    开始模拟正版软件

--- web/src/Router.tsx
+++ web/src/Router.tsx
@@ -1,17 +1,16 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import useHistory from './sz-support/common/hooks/useHistory';
 import { Router, Route, Switch } from 'react-router';
 import ProvisionalPass from './view/pages/ProvisionalPass';
 import NotFoundPage from './view/pages/NotFoundPage';
-import { showAboutAfterUpdated } from './common/util';
 import HomePage from "./view/pages/HomePage"
 //Application's router
 export default function AppRouter() {
 
     return <Router history={useHistory()}>
         <Switch>
-            {/* <Route exact path="/" component={HomePage}></Route> */}
-            <Route exact path="/" component={ProvisionalPass}></Route>
+            <Route exact path="/" component={HomePage}></Route>
+            {/* <Route exact path="/" component={ProvisionalPass}></Route> */}
             <Route exact path="/provisional-pass" component={ProvisionalPass}></Route>
             <Route path="*" component={NotFoundPage}></Route>
         </Switch>
