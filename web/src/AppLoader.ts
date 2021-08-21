7fd4774179678981babcdbcd690c76895755e5f1
zsh2401 <zsh2401@163.com>
2021-08-21 20:12:05 +0800

    暂时不禁用

--- web/src/AppLoader.ts
+++ web/src/AppLoader.ts
@@ -18,9 +18,9 @@ async function pwa() {
 }
 
 async function runApplication() {
-    alert("考虑到最新疫情防控形势，此软件暂时停止使用。\n只有安全，才能拥有美好的一天。\n就这样，随便吧，就算眼泪掉下来。")
-    // const app = await import("./App")
-    // app.default()
+    // alert("考虑到最新疫情防控形势，此软件暂时停止使用。\n只有安全，才能拥有美好的一天。\n就这样，随便吧，就算眼泪掉下来。")
+    const app = await import("./App")
+    app.default()
 }
 
 (async () => {
