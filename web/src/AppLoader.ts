e5511ac420a69b81ef3556dbeaa87dba852e285b
zsh2401 <zsh2401@163.com>
2021-08-21 20:08:21 +0800

    兼容老版本service-worker

--- web/src/AppLoader.ts
+++ web/src/AppLoader.ts
@@ -4,7 +4,7 @@ import nProgress from "nprogress"
 //Install service worker
 async function pwa() {
     try {
-        const registration = await navigator.serviceWorker.register('./sw.js')
+        const registration = await navigator.serviceWorker.register('/service-worker.js')
         console.log('SW registered: ', registration)
         registration.addEventListener("updatefound", async () => {
             console.log("Update found")
@@ -17,7 +17,7 @@ async function pwa() {
     }
 }
 
-async function runApplication(){
+async function runApplication() {
     alert("考虑到最新疫情防控形势，此软件暂时停止使用。\n只有安全，才能拥有美好的一天。\n就这样，随便吧，就算眼泪掉下来。")
     // const app = await import("./App")
     // app.default()
