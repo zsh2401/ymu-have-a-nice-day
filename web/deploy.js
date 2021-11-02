f31e69c685772dbcbb285d188447c84200e99098
zsh2401 <zsh2401@163.com>
2021-11-02 16:00:58 +0800

    deploy to coding master branch

--- web/deploy.js
+++ web/deploy.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 const ghpages = require("gh-pages")
 ghpages.publish("dist", {
+    branch: "master",
     repo: "https://e.coding.net/studio2401/toys/ymu-have-a-nice-day.git"
 },
     (err) => {
